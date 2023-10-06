58c32afafb4fbd8a29d934ba0a7546aa81a344bf
seotjuu <sjlee@moasoftware.co.kr>
2023-10-06 17:15:08 +0900

    [fix] views   - 단축 명령어 실행

--- polls/views.py
+++ polls/views.py
@@ -1,13 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
+
+from django.http import Http404
 
 # Create your views here.
 
+# Template과 view 구분해줘야함
+# def index(request):
+#     return HttpResponse("Hello, world.")
+
+# def index(request):
+#     latest_question_list = Question.objects.order_by("-pub_date")[:5]
+#     output = ", ".join([q.question_text for q in latest_question_list])
+#     return HttpResponse(output)
+
+# def index(request):
+#     latest_question_list = Question.objects.order_by("-pub_date")[:5]
+#     template = loader.get_template("polls/index.html")
+#     context = {
+#         "latest_question_list": latest_question_list,
+#     }
+#     return HttpResponse(template.render(context, request))
+
 def index(request):
-    return HttpResponse("Hello, world.")    # 출력 결과
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return render(request, "polls/index.html", context)
+
+# def detail(request, question_id):
+#     return HttpResponse("You're looking at question %s." % question_id)
+
+# def detail(request, question_id):
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist")
+#     return render(request, "polls/detail.html", {"question": question})
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id) # 질문 아이디가 없으면 404처리  # 예외처리
+    return render(request, "polls/detail.html", {"question": question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
